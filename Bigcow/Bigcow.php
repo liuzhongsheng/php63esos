482f4001e7763cf2f88c0db0e35354a5e42a1e1e
liuzhongsheng <996674366@qq.com>
2016-04-05 14:00:20 +0800

    删除无用代码
    删除无用代码

--- Bigcow/Bigcow.php
+++ Bigcow/Bigcow.php
@@ -15,6 +15,5 @@ const CLASS_SUFFIX          = '.class.php';
 define('SYSTEM_PATH')       OR      define('SYSTEM_PATH', __DIR__.'/');                              //定义系统目录
 define('APP_PATH')          OR      define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');   //定义应用目录路径
 define('APP_DEBUG')         OR      define('APP_DEBUG', false);                                     //是否开启调试模式默认否
-defined('APP_DEBUG')    or define('APP_DEBUG',      false); // 是否调试模式
 //defind('LIB_PATH')  = '
 //Core library'
